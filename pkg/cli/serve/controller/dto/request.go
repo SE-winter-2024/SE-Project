b82043bd1c9002fa804c00610241b56a2c113133
m.moghiseh <mahdiehmoghiseh81@gmail.com>
2024-05-25 23:31:34 +0330

    feat: edit profile

--- pkg/cli/serve/controller/dto/request.go
+++ pkg/cli/serve/controller/dto/request.go
@@ -76,20 +76,20 @@ type TrainerEdit struct {
 
 type UserEditTraineeOrTrainer struct {
 	User            UserEdit `json:"user"`
-	UserName        string   `json:"user_name" validate:"required"`
-	Status          string   `json:"status" validate:"required"`
-	CoachExperience uint     `json:"coach_experience" validate:"required"`
-	Contact         string   `json:"contact" validate:"required"`
-	Language        string   `json:"language" validate:"required"`
-	Country         string   `json:"country" validate:"required"`
-	Sport           string   `json:"sport" validate:"required"`
-	Achievements    string   `json:"achievements" validate:"required"`
-	Education       string   `json:"education" validate:"required"`
-	Height          uint     `json:"height" validate:"required"`
-	Weight          uint     `json:"weight" validate:"required"`
-	Sports          string   `json:"sports" validate:"required"`
-	MedicalHistory  string   `json:"medicalHistory" validate:"required"`
-	ActiveDays      []bool   `json:"active_days" validate:"required"`
+	UserName        string   `json:"user_name"`
+	Status          string   `json:"status"`
+	CoachExperience uint     `json:"coach_experience"`
+	Contact         string   `json:"contact"`
+	Language        string   `json:"language"`
+	Country         string   `json:"country"`
+	Sport           string   `json:"sport"`
+	Achievements    string   `json:"achievements"`
+	Education       string   `json:"education"`
+	Height          uint     `json:"height"`
+	Weight          uint     `json:"weight"`
+	Sports          string   `json:"sports"`
+	MedicalHistory  string   `json:"medicalHistory"`
+	ActiveDays      []bool   `json:"active_days"`
 }
 
 type TraineeEdit struct {
