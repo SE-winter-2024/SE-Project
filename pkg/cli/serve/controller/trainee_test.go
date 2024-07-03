bb112ce37c9ffeb59f1955958322fc9e72844114
m.moghiseh <m.moghiseh@jabama.com>
2024-07-04 02:55:26 +0330

    feat: add test

--- pkg/cli/serve/controller/trainee_test.go
+++ pkg/cli/serve/controller/trainee_test.go
@@ -1,14 +1,64 @@
-package serve
+package serve_test
 
 import (
-	"bitbucket.org/dyfrag-internal/mass-media-core/pkg/cli/serve/controller/dto"
+	serve2 "bitbucket.org/dyfrag-internal/mass-media-core/pkg/cli/serve/controller"
 	"bytes"
-	"encoding/json"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+
+	"bitbucket.org/dyfrag-internal/mass-media-core/pkg/cli/serve/controller/dto"
+	"encoding/json"
+	"github.com/gavv/httpexpect/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestTraineeControllerEndpoints(t *testing.T) {
+	app := fiber.New()
+
+	controller := &serve2.TraineeController{}
+	api := app.Group("/trainee")
+	controller.RegisterRoutes(api)
+
+	os.Setenv("JWT_SECRET_KEY", "your_secret_key")
+
+	t.Run("EditProfile", func(t *testing.T) {
+		reqBody := dto.TraineeEdit{}
+		token := createTestToken("1")
+		e := httpexpect.WithConfig(httpexpect.Config{
+			BaseURL:  "http://localhost",
+			Reporter: httpexpect.NewAssertReporter(t),
+		})
+		e.PUT("/trainee/profile/1").
+			WithHeader("Authorization", "Bearer "+token).
+			WithJSON(reqBody).
+			Expect().
+			Status(http.StatusOK).JSON().Object().ContainsKey("UserName")
+	})
+
+	t.Run("GetTraineeProfile", func(t *testing.T) {
+		token := createTestToken("1")
+		e := httpexpect.WithConfig(httpexpect.Config{
+			BaseURL:  "http://localhost", // Set your base URL here
+			Reporter: httpexpect.NewAssertReporter(t),
+		})
+		e.GET("/trainee/profile/1").
+			WithHeader("Authorization", "Bearer "+token).
+			Expect().
+			Status(http.StatusOK).JSON().Object().ContainsKey("UserName")
+	})
+}
+
+func createTestToken(userID string) string {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["user_id"] = userID
+	tokenString, _ := token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
+	return tokenString
+}
 
 func TestCreateProgramRequest(t *testing.T) {
 	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1haGRpZWhtb2doaXNlaDgxdHJhaW5lZTJAZ21haWwuY29tIiwiZXhwIjoxNzE2NzU1Mjk2LCJ1c2VyX2lkIjoxfQ.IizpOZ5At3WcwYdR6jK4tGE8eDeRSKvJlT8FqBkcPnE"
--- pkg/cli/serve/controller/trainer_test.go
+++ pkg/cli/serve/controller/trainer_test.go
@@ -1,11 +1,63 @@
-package serve
+package serve_test
 
 import (
+	serve2 "bitbucket.org/dyfrag-internal/mass-media-core/pkg/cli/serve/controller"
+	"bitbucket.org/dyfrag-internal/mass-media-core/pkg/cli/serve/controller/dto"
+	"bytes"
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+func setupTestApp() (*fiber.App, *serve2.TrainerController) {
+	app := fiber.New()
+	trainerCtrl := &serve2.TraineeController{}
+	trainerGroup := app.Group("/trainer") // Mimic the group used in the controller
+	trainerCtrl.RegisterRoutes(trainerGroup)
+	return app, nil
+}
+
+func TestEditProfile(t *testing.T) {
+	app, _ := setupTestApp()
+
+	editProfileData := dto.TrainerEdit{}
+	body, _ := json.Marshal(editProfileData)
+
+	req := httptest.NewRequest(http.MethodPut, "/trainer/profile/", bytes.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "mockJWTToken") // Set a mock JWT token for authorization
+
+	resp, err := app.Test(req)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	var response dto.TrainerResponse
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	assert.NoError(t, err)
+
+}
+
+func TestGetTrainerProfile(t *testing.T) {
+	app, _ := setupTestApp()
+
+	req := httptest.NewRequest(http.MethodGet, "/trainer/profile/", nil)
+	req.Header.Set("Authorization", "mockJWTToken") // Set a mock JWT token for authorization
+
+	resp, err := app.Test(req)
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	var response dto.TrainerResponse
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	assert.NoError(t, err)
+}
 
 func TestTrainerController_GetAllRequests(t *testing.T) {
 	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1haGRpZWhtb2doaXNlaDgxdHJhaW5lcjJAZ21haWwuY29tIiwiZXhwIjoxNzE2NzU1MzI3LCJ1c2VyX2lkIjoyfQ.EhOIyG5fUDM3aP7g11aUu98QDiEsrVs9yCWKi8EklD8"
